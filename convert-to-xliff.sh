93f5ceb3b58d7d9659cd09ce43a0d1ad0fbdbcae
Jeremy Lanssiers <jlanssie@gmail.com>
2021-10-21 00:15:38 +0200

    2021-10-21 -- 00:15:38

--- convert-to-xliff.sh
+++ convert-to-xliff.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
-# FUNCTION: Convert csv to xliffs
+# FUNCTION: Convert csv to (AEM-specific) xliffs
 # ARGUMENTS: /
 # AUTHOR: Jeremy Lanssiers
-# VERSION: 0.1
-# REQUIRES: zsh, bash and a file in the same directory called input.csv with semicolon delimiters, a first column that contains the key string and a second, third, fourth and fifth column for the language values in EN, NL, FR, DE, in that order.
+# VERSION: 1.0
+# REQUIRES: zsh/bash and a file in the same directory called input.csv with semicolon delimiters, a first column that contains the key string and one or mulitple columns with values. The first row should contain the languages, e.g.: key;EN;ES;CH;IN
 
-DATE=$(date)
-LANGUAGES=(nl fr de)
+INPUT="./input.csv"
 
-INDEX=0
+LANGUAGES=()
+
+INDEX=1 #Index 0 = Key. Index 1+ = Values
+
+while IFS=";" read -a A
+do
+    for ((i=$INDEX; i<${#A[@]}; i++))
+    do
+        LANGUAGES+=($( echo ${A[$i]} | tr '[:upper:]' '[:lower:]' | tr -d '\r' ))
+    done
+done < <(echo $(head -n 1 $INPUT )) #Input
 
 for LANGUAGE in "${LANGUAGES[@]}"
 do
+    echo -e "processing $LANGUAGE\n..."
+
     ID=0
     OUTPUT=""
-    OUTPUT+="<xliff version=\"1.1\"><file original=\"/libs/cq/i18n/"$LANGUAGE"\" source-language=\"en\" target-language=\""$LANGUAGE"\" datatype=\"x-javaresourcebundle\" tool-id=\"com.day.cq.cq-i18n\" date=\""$DATE"\">\n<header>\n<tool tool-id=\"com.day.cq.cq-i18n\" tool-name=\"Adobe Granite I18N Module\" tool-version=\"5.5.16\" tool-company=\"Adobe Systems Incorporated\"/>\n</header>\n<body>\n"
+    OUTPUT+="<xliff version=\"1.1\"><file original=\"/libs/cq/i18n/"$LANGUAGE"\" source-language=\"en\" target-language=\""$LANGUAGE"\" datatype=\"x-javaresourcebundle\" tool-id=\"com.day.cq.cq-i18n\" date=\""$(date)"\">\n<header>\n<tool tool-id=\"com.day.cq.cq-i18n\" tool-name=\"Adobe Granite I18N Module\" tool-version=\"5.5.16\" tool-company=\"Adobe Systems Incorporated\"/>\n</header>\n<body>\n"
     
-    while IFS=";" read -r col1 col2 col3 col4 col5 #The number of columns that will be read
-    #while IFS=";" read -a A
+    while IFS=";" read -a A
     do
-        KEY=$col1
-        EN=$col2
-        NL=$col3
-        FR=$col4
-        DE=$col5
-        VALUES=("$NL" "$FR" "$DE")
-        VALUE=$( echo ${VALUES[$INDEX]})
+        KEY=${A[0]}
+        VALUE=$( echo ${A[$INDEX]} | tr -d '\r')
 
         ITEM=""
-        ITEM+=$( echo -e "<trans-unit id=\""$ID"\">\n" );
-        ITEM+=$( echo -e "<source xml:lang=\"en\">\n<![CDATA[ "$KEY" ]]>\n</source>\n" )
-        ITEM+=$( echo -e "<target xml:lang=\""$LANGUAGE"\">\n<![CDATA[ "$VALUE" ]]>\n</target>\n" )
-        ITEM+=$( echo -e "</trans-unit>\n" )
-
-        OUTPUT+=$(echo -e $ITEM)
-        OUTPUT+="\n"
+        ITEM+=$( echo "<trans-unit id=\""$ID"\">" );
+        ITEM+=$( echo "<source xml:lang=\"en\">\n<![CDATA[ "$KEY" ]]>\n</source>" )
+        ITEM+=$( echo "<target xml:lang=\""$LANGUAGE"\">\n<![CDATA[ "$VALUE" ]]>\n</target>" )
+        ITEM+=$( echo "</trans-unit>" )
+        OUTPUT+=$( echo -e "$ITEM\r" )
 
         ID=$((ID+1))
-
-    done < input.csv #input csv
+    #done < <(echo $(tail -n +1 $INPUT )) #Input
+    done < $INPUT 
 
     OUTPUT+="</body>\n</file>\n</xliff>"
-
-    echo -e $OUTPUT > ./$LANGUAGE.xliff #output txt
+    echo -e $OUTPUT > ./"$LANGUAGE".xliff #Output
 
     INDEX=$((INDEX+1))
+
+    echo -e "completed $LANGUAGE\n"
 done
 
 exit 0
